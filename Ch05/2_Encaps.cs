fdf32a584c07c611f0581dc82fb0b71b0b33ada4
noranfox <sadpo5660@gmail.com>
2022-06-08 12:18:23 +0900

    1234

---
+++ Ch05/2_Encaps.cs
@@ -0,0 +1,58 @@
+﻿using Ch05.Sub2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 날짜 : 2022 /06/08
+ * 이름 : 정현기
+ * 내용 : 캡슐화 실습하기 교재 p203
+ *  
+ */
+
+namespace Ch05
+{
+    internal class _2_Encaps
+    {
+        static void Main(string[] args)
+        {
+           Account kb = new Account("국민은행", "123-12-1234", "김유신", 1000);
+     
+
+
+            kb.Deposit(4000);
+            kb.Withdraw(3000);
+          
+            //캡술화를 하지 않아, 취약점 발생
+           // kb.balance--;
+            // kb.balance -= 10;
+            
+            
+            
+            kb.Show();
+
+            Car sonata = new Car("소나타", "흰색", 0);
+            Car BMW = new Car("BMW", "검정색", 0);
+            Car Benz = new Car("벤츠", "검정색", 0);
+
+            //setter 실행
+            sonata.Name = "EF소나타";
+            sonata.Speed = -100;
+
+            //getter 실행
+            Console.WriteLine("sonata 객체 name : " + sonata.Name);
+
+
+            sonata.SpeedUp(60);
+            sonata.Show();
+
+            BMW.SpeedUp(50);
+            BMW.Show();
+
+            Benz.SpeedUp(10);
+            Benz.Show();
+
+        }   
+    }
+}
---
+++ Ch05/Sub2/Car.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch05.Sub2
+{
+    internal class Car
+    {
+        //속성(필드-변수)
+        private string name;
+        private string color;
+        private int speed;
+
+        //Getter, Setter -->C#에서는 프로퍼티
+        public string Name { get {return name;} set { name = value; }}
+        public string Color { get {return color;} set {color = value;} }
+        public int Speed 
+        { 
+            get 
+            {
+                return speed;
+            }
+            set 
+            {
+                if (value < 0)
+                {
+                    speed = 0;
+                }
+                else if (value > 300)
+                {
+                    value = 300;
+                }
+                else
+                {
+                    speed = value;
+                }
+            }
+        }
+
+
+        //생성자
+        public Car(string name, string color, int speed)
+        {
+            this.name = name;
+            this.color = color;
+            this.speed = speed;
+        }
+
+        //소멸자  ~생산자() 표시
+        ~Car()
+        {
+            Console.WriteLine("{0}Car 소멸자 실행...", name);
+        }
+
+    
+
+        //기능(메서드)
+        public void SpeedUp(int speed)
+        {
+            this.speed += speed; //this.는 class에 변수 라는것을 표현
+        }
+        public void SpeedDown(int speed)
+        {
+            this.speed -= speed;
+        }
+        public void Show()
+        {
+            Console.WriteLine("============");
+            Console.WriteLine("차량명 : "+this.name);
+            Console.WriteLine("차량색 : "+ this.color);
+            Console.WriteLine("차량속도 : "+ this.speed);
+        }
+    }
+}
