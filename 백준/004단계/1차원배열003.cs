664e2f84d40e27abc7bad758592fd33524821e6a
noranfox <sadpo5660@gmail.com>
2022-06-13 16:18:33 +0900

    22/6/13 database trining1

--- 백준/004단계/1차원배열003.cs
+++ 백준/004단계/1차원배열003.cs
@@ -13,7 +13,7 @@ namespace 백준._004단계
         static void Main(string[] args)
         {
             //A*B*C=D
-            //D값에 0~9까지 쓰인 숫자 카운드 D= 1000 0:3 1:1
+            //D값에 0~9까지 쓰인 숫자 카운드 D= 1000 -> 3 1 , D=1135423 ->0 2 1 2 1 1
 
             string num1 = Console.ReadLine();
             string num2 = Console.ReadLine();
@@ -26,16 +26,26 @@ namespace 백준._004단계
             
             int D = A * B * C;
             string total = D.ToString();
-            string[] Total = total.Split('\u002C');
-            Console.WriteLine(D);
-
-            foreach(string item in Total)
-            { 
-                Console.WriteLine(item+); 
-            }
-            //D 배열을 카운트
+            int[] Total = new int[total.Length];
             
+            while (D > 0)
+             {
+                if (int i = 0; i < total.Length; i++ )
+                {   Total[i] = D % 10;
+                    D /= 10;
+                }
+             }
+            foreach(int item in Total)
+            {
+                Console.WriteLine("item[0]",item +item);
+            }
 
+    
+            //D 배열을 카운트
+            //1,2,3,4,5,6,7,8,9
+            //1,2,3, 2*2,2*3,2*2*2,3*3
+            
+            
 
         }
     }
