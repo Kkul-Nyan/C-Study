fbe3ac7da7a452a970c014cabb93258c6ffbc276
noranfox <sadpo5660@gmail.com>
2022-06-15 17:20:21 +0900

    22/06/15 finish

--- 백준/003단계/반복문 008.cs
+++ 백준/003단계/반복문 008.cs
@@ -8,5 +8,26 @@ namespace 백준._003단계
 {
     internal class 반복문_008
     {
+        static void Main1(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            
+            int[] number = new int[T];
+            int[] number1 = new int[T];
+
+            for (int j = 0; j < T; j++)
+            {
+                string[] str = Console.ReadLine().Split();
+                number[j] = int.Parse(str[0]);
+                number1[j] = int.Parse(str[1]);
+            }
+
+            Console.Clear();
+
+            for(int i =0; i < T; i++)
+            {
+                Console.WriteLine("Case #"+(i+1)+": " + ""+number[i]+" + " + number1[i] + " = " + (number[i] + number1[i]));
+            }
+        }
     }
 }
--- 백준/003단계/반복문 009.cs
+++ 백준/003단계/반복문 009.cs
@@ -8,5 +8,20 @@ namespace 백준._003단계
 {
     internal class 반복문_009
     {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            Console.Clear();
+            for(int i = 1; i < T+1 ; i++)
+            {
+                int j = 0;
+                while (j != i)
+                {
+                    Console.Write("*");
+                    j++;
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
