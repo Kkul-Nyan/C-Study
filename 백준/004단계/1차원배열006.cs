940b2401bab7444a959642bc57121952b9afbdd6
noranfox <sadpo5660@gmail.com>
2022-06-19 22:27:48 +0900

    22/06/19 study

--- 백준/004단계/1차원배열006.cs
+++ 백준/004단계/1차원배열006.cs
@@ -4,64 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//일단 반복 횟수 T를준다 => T만큼 반복하게 for 반복문 1회
+//OX를 구분할 반복문
+//OX를 O그룹에 숫자를 대입 계산하는 반복문
+//OX 한줄에 O그룹을 합치는 반박문
+//출력
+//  반복횟수 입력받음 / 전체반복 [ OX구분 [ o그룹 총합 [ O그룹 계산 ] 각라인값 출력]]]
+
+//1번계획 실패
+//O만 독자적인 그룹 형성 과정 만들기 실패
+//X를 int값 초기화 시키게 만들어서 o계산식을 초기화해서 X나올때마다 1,2,3으로 만들어버리자
+
 namespace 백준._004단계
 {
     internal class _1차원배열006
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
+            
             int t = int.Parse(Console.ReadLine());
-            string[] str = Console.ReadLine().Split();
-            char[] ch = str[0].ToCharArray();
-            int final = 0;
-            //t로 총 반복문을 몇번할건지 확정
-            for (int i = 0; i < t; i++)
+            int[] Final = new int[t];
+
+            for (int i = 0; i < t; i++) // 전체 반복문
             {
+                string[] str = Console.ReadLine().Split();                                                    // OX입력받을 string 그룹
+                char[] ch = str[0].ToCharArray();                                                             // 반복횟수 입력받고 OX입력받음 OX char그룹으로 낱개 분할
+                             
                 int num = 0;
-                int Total = 0;
-                int count = 0;
+                int [] Onum = new int[ch.Length]; 
                 for (int j = 0; j < ch.Length; j++)
                 {
-                    Console.Write(ch[0] + " "); Console.WriteLine(ch[1]);
-                    while (ch[count] ==  )
+                    if (ch[j] == 'X')
                     {
-                        count ++;
+                         num = 0;
+                         Onum[j] = 0;
                     }
-                    for(int q=0; q < ch.Length; q++)
-                    { 
-                        int[] nums = new int[count];
-                    
-                        Console.WriteLine("count : "+count);
-                    
-                        for (int k = 0; k < count; k++)
-                        {
-                            num += 1;
-                            nums[k] = num;
-                        }
-                        int total = 0;
 
-                         foreach(int a in nums)
-                        { Console.Write("nums 그룹 : "+a); }
-                    
-                        for (int u = 0; u < count; u++)
-                        {
-                        total += nums[u];
-                        Console.WriteLine("total"+total);
-                        }
-                     Total += total;
-                     Console.WriteLine("Total" + Total);
+                    //OOOO     OXOXOXO    OOXOOOXOOOO  OOOOXXXOOXOOOXX
+                    //1+2+3+4
+
+                    else
+                    {
+                        Onum[j] = num + 1;
+                        num++;
                     }
 
                 }
-                final += Total;
-                Console.WriteLine("final"+final);
+                int total = 0;
+                for (int H=0; H<Onum.Length; H++)
+                {
+                    total +=Onum[H];
+                }
+                
+                Final[i] = total;
+                
             }
-
-            
-        
-        
-        
-        
+            Console.Clear();
+            foreach(int F in Final)
+            { Console.WriteLine(F); }
         }
     }
 }
--- 백준/004단계/1차원배열007.cs
+++ 백준/004단계/1차원배열007.cs
@@ -8,6 +8,44 @@ namespace 백준._004단계
 {
     internal class _1차원배열007
     {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            double[] final = new double[T];
+            for (int i = 0; i < T; i++)
+            {
+                string[] str = Console.ReadLine().Split();
+                double[] points = new double[str.Length];
+                for(int k =0 ; k < str.Length; k++)
+                {
+                    points[k] = int.Parse(str[k]);
+                }
+                double total = 0;
+                for(int j = 1 ; j < points.Length; j++)
+                {
+                    total += points[j];
+                }
+                double average = total / points[0];
 
+                double count = 0;
+                for (int h = 1; h < str.Length; h++)
+                {
+                    if (points[h] > average)
+                    {
+                        count++;
+                    }
+
+                }
+                double people_age = (count / points[0]) * 100; ;
+                
+                final[i] = Math.Round(people_age, 3);
+
+            }
+            Console.Clear();
+            foreach (double k in final)
+            {
+                Console.WriteLine(k+"%");
+            }
+        }
     }
 }
